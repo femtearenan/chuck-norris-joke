750a51693b22c727bce6220382f3596214df9123
Minerva <minerva@femtearenan.se>
2018-09-26 14:51:40 +0200

    An efficiency modification: Create just one ChuckJokeService object.

--- src/main/java/se/femtearenan/chucknorrisjoke/services/ChuckJokeService.java
+++ src/main/java/se/femtearenan/chucknorrisjoke/services/ChuckJokeService.java
@@ -5,9 +5,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ChuckJokeService implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public ChuckJokeService() {
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String getJoke() {
-        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
         return chuckNorrisQuotes.getRandomQuote();
     }
 }
